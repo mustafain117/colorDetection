d1278f1fa6c3e26684d23cce372de5c7dae2688d
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2020-02-24 20:43:21 -0500

    quickfix

--- src/colorDetection/ColorDetector.java
+++ src/colorDetection/ColorDetector.java
@@ -3,17 +3,16 @@ import static colorDetection.Resources.*;
 
 public class ColorDetector implements Runnable{
 
-  private static final long COLOUR_PERIOD = 1000;
-  private long timeout = Long.MAX_VALUE;
+  private static final long PERIOD = 1000;
+
 
   //local variables for 
   public  float colourRed;
   public  float colourGreen;
   public  float colourBlue;
-  //for some reason this sampleProvider does not work.
-  //private static SampleProvider colourValue = FRONT_COL_SENSOR.getRGBMode(); 
+
   /**
-   * Buffer (array) to store light samples.
+   * to store rgb samples.
    */
   static float[] colourData = new float[3];
 
@@ -51,9 +50,9 @@ public class ColorDetector implements Runnable{
       updateRingColour(normRed, normGreen, normBlue);
       // this ensures that the light sensor runs once every 3 ms.
       updateDuration = System.currentTimeMillis() - updateStart;
-      if (updateDuration < COLOUR_PERIOD) {
+      if (updateDuration < PERIOD) {
         try {
-          Thread.sleep(COLOUR_PERIOD - updateDuration);
+          Thread.sleep(PERIOD - updateDuration);
         }catch(InterruptedException e) {
           
         }
@@ -62,7 +61,7 @@ public class ColorDetector implements Runnable{
   }
 
 
-  public  COLOUR updateRingColour(double colourRed, double colourGreen, double colourBlue) {
+  public  void updateRingColour(double colourRed, double colourGreen, double colourBlue) {
     this.ringColour = COLOUR.NONE;
     
     int stdDevs = 40;
@@ -70,57 +69,51 @@ public class ColorDetector implements Runnable{
     int stDevsOY = 30;
     
     if ((colourBlue >= GREEN_MEAN[2] - stdDevs* GREEN_SD[2]) & (colourBlue <= GREEN_MEAN[2] + stdDevs*GREEN_SD[2])) {
-//      System.out.println("g1\n");
+
       if ((colourRed >= GREEN_MEAN[0] - stdDevs*GREEN_SD[0]) & (colourRed <= GREEN_MEAN[0] + stdDevs*GREEN_SD[0])) {
-//        System.out.println("g2\n");
+
         if ((colourGreen >= GREEN_MEAN[1] - stdDevs*GREEN_SD[1]) & (colourGreen <= GREEN_MEAN[1] +stdDevs* GREEN_SD[1])) {
-//          System.out.println("g3\n");
+
           this.ringColour = COLOUR.GREEN;
         }
       }
     } 
 
-    if ((colourBlue >= BLUE_MEAN[2] - stdDevs*BLUE_SD[2]) & (colourBlue <= BLUE_MEAN[2] +stdDevs* BLUE_SD[2])) {
-      //Printer.printMessage("b1\n", 2);
-//      System.out.println("b1\n");
-      if ((colourRed >= BLUE_MEAN[0] -stdDevs* BLUE_SD[0]) & (colourRed <= BLUE_MEAN[0] +stdDevs* BLUE_SD[0])) {
-        //Printer.printMessage("b2\n", 3);
-//        System.out.println("b2\n");
-        if ((colourGreen >= BLUE_MEAN[1] - stdDevs*BLUE_SD[1]) & (colourGreen <= BLUE_MEAN[1] +stdDevs* BLUE_SD[1])) {
-          //Printer.printMessage("b3\n", 4);
-//          System.out.println("b3\n");
+    if ((colourBlue >= BLUE_MEAN[2] - stdDevs*BLUE_SD[2]) && (colourBlue <= BLUE_MEAN[2] +stdDevs* BLUE_SD[2])) {
+
+      if ((colourRed >= BLUE_MEAN[0] -stdDevs* BLUE_SD[0]) && (colourRed <= BLUE_MEAN[0] +stdDevs* BLUE_SD[0])) {
+
+        if ((colourGreen >= BLUE_MEAN[1] - stdDevs*BLUE_SD[1]) && (colourGreen <= BLUE_MEAN[1] +stdDevs* BLUE_SD[1])) {
+
           this.ringColour = COLOUR.BLUE;
         }
       }
     } 
     
 
-    if ((colourBlue >= YELLOW_MEAN[2] - stDevsOY*YELLOW_SD[2]) & (colourBlue <= YELLOW_MEAN[2] + stDevsOY*YELLOW_SD[2])) {
-//      System.out.println("y1\n");
-      if ((colourRed >= YELLOW_MEAN[0] - stDevsOY*YELLOW_SD[0]) & (colourRed <= YELLOW_MEAN[0] + stDevsOY*YELLOW_SD[0])) {
-//        System.out.println("y2\n");
-        if ((colourGreen >= YELLOW_MEAN[1] - stDevsOY*YELLOW_SD[1]) & (colourGreen <= YELLOW_MEAN[1] +stDevsOY* YELLOW_SD[1])) {
-//          System.out.println("y3\n");
+    if ((colourBlue >= YELLOW_MEAN[2] - stDevsOY*YELLOW_SD[2]) && (colourBlue <= YELLOW_MEAN[2] + stDevsOY*YELLOW_SD[2])) {
+
+      if ((colourRed >= YELLOW_MEAN[0] - stDevsOY*YELLOW_SD[0]) && (colourRed <= YELLOW_MEAN[0] + stDevsOY*YELLOW_SD[0])) {
+
+        if ((colourGreen >= YELLOW_MEAN[1] - stDevsOY*YELLOW_SD[1]) && (colourGreen <= YELLOW_MEAN[1] +stDevsOY* YELLOW_SD[1])) {
+
           this.ringColour = COLOUR.YELLOW;
         }
       }
 
     }  
-    if ((colourBlue >= ORANGE_MEAN[2] - stdDevs*ORANGE_SD[2]) & (colourBlue <= ORANGE_MEAN[2] + stdDevs*ORANGE_SD[2])) {
-//      System.out.println("o1\n");
-      if ((colourRed >= ORANGE_MEAN[0] - stdDevs*ORANGE_SD[0]) & (colourRed <= ORANGE_MEAN[0] + stdDevs*ORANGE_SD[0])) {
-//        System.out.println("o2\n");
-        if ((colourGreen >= ORANGE_MEAN[1] - stdDevs*ORANGE_SD[1]) & (colourGreen <= ORANGE_MEAN[1] + stdDevs*ORANGE_SD[1])) {
-//          System.out.println("o3\n");
+    if ((colourBlue >= ORANGE_MEAN[2] - stdDevs*ORANGE_SD[2]) && (colourBlue <= ORANGE_MEAN[2] + stdDevs*ORANGE_SD[2])) {
+
+      if ((colourRed >= ORANGE_MEAN[0] - stdDevs*ORANGE_SD[0]) && (colourRed <= ORANGE_MEAN[0] + stdDevs*ORANGE_SD[0])) {
+
+        if ((colourGreen >= ORANGE_MEAN[1] - stdDevs*ORANGE_SD[1]) && (colourGreen <= ORANGE_MEAN[1] + stdDevs*ORANGE_SD[1])) {
+
           this.ringColour = COLOUR.ORANGE;
         }
       }
     }
     this.color = this.ringColour + "";
-    return this.ringColour;
-
   }
-
 
   public static String getColor() {
     return color;
